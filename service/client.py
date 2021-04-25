87882c62a4d6851f827af810cde3ef3f1fe996c0
Israel <se.israel.abebe@gmail.com>
2021-04-26 01:04:38 +0300

    corrected naimg issue

--- service/client.py
+++ service/client.py
@@ -7,12 +7,12 @@ import grpc
 from service_spec import fake_news_pb2
 from service_spec import fake_news_pb2_grpc
 
-channel = grpc.insecure_channel('109.88.2.12:7011')
+channel = grpc.insecure_channel('localhost:7011')
 
 stub = fake_news_pb2_grpc.fake_news_classificationStub(channel)
 
 input_text = fake_news_pb2.InputMessage(value="This is sample text ")
 
-response = stub.predict(input_text)
+response = stub.classify(input_text)
 
 print(response.result)
